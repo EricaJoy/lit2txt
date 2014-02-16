e40e069d2793b6a5e6da71dd5fb22cdb60587168
Erica Joy <git@ericabaker.com>
2014-02-15 16:49:39 -0800

    Now prepends the story with the title and author.

--- lit2txt.py
+++ lit2txt.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 import sys
 import re
 import urllib2
@@ -10,13 +12,20 @@ def get_html(url):
 
     return raw_html
 
-def clean_text(txt):
+def clean_text(txt, first_pass = False):
     
-    # Create the expression to isolate the story
-    source = re.compile('<div class="b-story-body-x x-r15"><div><p>([\w\s\W]*)</p></div></div><div class="b-story-stats-block">')
+    # Create the expression to isolate the title, author, and story
+    source = re.compile('<div class="b-story-header"><h1>([\w\s\W]*)</h1>'
+                        '[\w\s\W]*<!-- ! --></span><a href=".*">([\w\s\W]*)'
+                        '</a></span><span class="b-story-copy">©</span></div>'
+                        '<div class="b-story-body-x x-r15"><div><p>([\w\s\W]*)'
+                        '</p></div></div><div class="b-story-stats-block">')
     
-    # Isoate the story
-    pulled_text = source.findall(txt)[0]
+    # Isolate the title, author, story
+    matches = source.findall(txt)
+    title = matches[0][0]
+    author = matches[0][1]
+    pulled_text = matches[0][2]
     
     # Create the expression to isolate remaning line breaks tags
     br = re.compile('<br  />')
@@ -32,16 +41,32 @@ def clean_text(txt):
     bold = re.compile('<[bB/]*>')
     cleaned_text = bold.sub('**', cleaned_text)
 
-    return cleaned_text
+    # If we are building page 1 of the story, return author, title, and text
+    if first_pass:
+        return title + '\n\n© ' + author + '\n\n' + cleaned_text
 
-def text_builder(url,output_text):
+    # If we are not building page one, just return the text
+    else:
+        return cleaned_text
+
+# Function to build the the text with URLs
+def text_builder(url,output_text,first_pass = False):
+
+    # Call the get_html function to return raw html
     txt = get_html(url)
-    cleaned = clean_text(txt)
+
+    # Send the raw html to the clean_text function to make it pretty
+    cleaned = clean_text(txt,first_pass)
+
+    # Append the cleaned text to the output text
     output_text = output_text + '\n\n' + cleaned
 
+    # Send back the cleaned and concatted output text
     return output_text
 
 def main():
+
+    # Take in the arguments from the command line and assign them to variables
     args = sys.argv
     rooturl = args[1]
     numpages = int(args[2])
@@ -50,16 +75,16 @@ def main():
     output_text = "Text version of " + rooturl + "\n\n"
 
     # Build the text for the root page
-    output_text = text_builder(rooturl, output_text)
+    output_text = text_builder(rooturl, output_text, first_pass = True)
     
     # Build the URL for each page after the first
     for i in range(2,numpages+1):
         url = rooturl + '?page=' + str(i)
         output_text = text_builder(url, output_text)
-        
-
 
     # Eventually save to a file, for now print
+
+
     print output_text
 
 if __name__ == "__main__":
